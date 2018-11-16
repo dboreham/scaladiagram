0e23f39a00bf8d828498d0f3bebb2f8f693c7716
stanfen <stanfen@126.com>
2018-11-16 17:31:48 +0800

    Update Builder.scala

--- src/main/scala/muyan/Builder.scala
+++ src/main/scala/muyan/Builder.scala
@@ -1,11 +1,12 @@
 package muyan
 
-import muyan.diagramsketch.DiagramSketch
+import muyan.diagramsketch.{DiagramLite, DiagramSketch}
 import muyan.graphviz.Graphviz
-import muyan.template.{DigraphBase, EdgeGraph}
+import muyan.template.DigraphBase
+import muyan.template.EdgeGraph._
 
 class Builder(path: String) {
- self: DiagramSketch with Graphviz with DigraphBase =>
+  self: DiagramSketch with Graphviz with DigraphBase =>
   //todo check path is valid or not
   def isValid : Boolean = {
     //dir file
@@ -16,10 +17,13 @@ class Builder(path: String) {
 
   //todo parse function innerclass inherit class etc.
   //todo SketchNode add parsing function, every class override it.
-  val dotBuilder =  sketch.foreach{ el =>
-   val name = el.clz.name.rawText
-
+  val dotBuilder =  sketch.foreach{ case DiagramLite(clz, ext, inner, method) =>
+    addItem(clz.descSketch, method.map( e => s"${e.descSketch}").mkString("\n"),"")
+    if(ext.isDefined) addRelation(clz.descSketch, ext.get.descSketch, inherit)
+    inner.foreach {el =>
+     addRelation(clz.descSketch, el.descSketch, composition)}
   }
+
 
 
 
