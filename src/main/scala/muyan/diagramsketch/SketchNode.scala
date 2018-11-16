0d521ad165b1c7bd951154e8e5495b58cbd56626
stanfen <stanfen@126.com>
2018-11-16 17:33:05 +0800

    Update SketchNode.scala

--- src/main/scala/muyan/diagramsketch/SketchNode.scala
+++ src/main/scala/muyan/diagramsketch/SketchNode.scala
@@ -8,31 +8,37 @@ sealed trait SketchNode {
   implicit def toOption[A <: SketchNode](in: A) : Option[A] = Some(in)
   implicit def isOption[A <: SketchNode](in: Option[A]) : Boolean = in.isDefined
   var deepLength = 0
-  def descSketch: List[String]
+  def descSketch: String
 }
 
 case class ClazzSketch(marker: List[Token], name: Token) extends SketchNode {
-  override def descSketch: List[String] = {
-//    val prefix = marker.map(_.rawText).mkString(" ")
-    List(name.rawText)
+  override def descSketch: String = {
+    name.rawText
   }
 }
 //todo scan inherit class para and class body info
+//todo class define need modify for option nested with diagram lite
+// return TemplateParents not List[Token]
 case class InheritSketch(op: Option[List[Token]]) extends SketchNode {
-  override def descSketch: List[String] = {
-
-   if(op.isDefined) op.get.map(_.text)  else Nil
+  override def descSketch: String = {
+   if(op.isDefined) op.get.map(_.text).mkString  else ""
   }
 }
 
 case class Slash(slh: Token) extends SketchNode {
-  override def descSketch: List[String] = Nil
+  override def descSketch: String = ""
 }
 
 case class FunctionSketch(fun: List[Token], param: ParamClauses ,returnType: Option[(Token, Type)]) extends SketchNode {
-  override def descSketch: List[String] = {
+  override def descSketch: String = {
     val name = fun.filter(_.tokenType.eq(Tokens.VARID)).map(_.text).mkString
-    List(name)
+    val paraList = param.tokens.map(_.text).mkString
+    val ret =  if (returnType.isDefined)  {
+      val v = returnType.get
+      s"${v._1.text}${v._2.tokens.map(_.text).mkString}"
+    } else ""
+
+    s"$name$paraList$ret"
   }
 }
 
@@ -41,8 +47,5 @@ case class FunctionSketch(fun: List[Token], param: ParamClauses ,returnType: Opt
                        var ext: Option[InheritSketch] = None,
                        var inner: List[ClazzSketch] = Nil,
                        var fun: List[FunctionSketch] = Nil) //extends SketchNode
-
-
-
 
 
