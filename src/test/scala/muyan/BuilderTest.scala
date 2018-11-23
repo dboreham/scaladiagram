b5577d2cddf5b99c3bc3eacfb5a6926041f94387
stanfen <stanfen@126.com>
2018-11-23 22:29:24 +0800

    Update BuilderTest.scala

--- src/test/scala/muyan/BuilderTest.scala
+++ src/test/scala/muyan/BuilderTest.scala
@@ -14,32 +14,11 @@ import scala.util.Try
 class BuilderTest extends FlatSpec with ShouldMatchers  {
 
   "muyan" should "test Builder function about dot string and sketch result" in {
-    val path = "M:\\BIGDATA\\SourceCode\\wayne\\src\\main\\scala\\com\\wayne\\future\\PerformManagement.scala"
+
 //    val path = "M:\\BIGDATA\\SourceCode\\scaladiagram\\src\\main\\scala\\muyan\\Builder.scala"
     val builder = new Builder(path)
 
     builder.build
-
-//    val in = builder.sourceParser(builder.readFile(path))
-//    //    in.topStats.otherStats.map(_._2).foreach(extractToken(_))
-//    in.topStats.otherStats.foreach(x => builder.extractToken(x._2))
-//    builder.fileSketch.foreach {
-//      el =>
-//        println("* " + el.clz)
-//        println("[]"+ el.clz.descSketch)
-//        println("--------------")
-//        println("* " + el.ext)
-//        if (el.ext.isDefined) println(el.ext.get.descSketch)
-
-//        println("--------------")
-//        println("* " + el.inner)
-//        println("[]"+ el.inner.foreach(_.descSketch))
-//        println("--------------")
-//        println("* " + el.fun)
-//        println("[]"+ el.fun.foreach(_.descSketch))
-//        println("--------------")
-//    }
-
 
   }
 
