4933818421bbd57cc7e47d174a733343d30d71b8
stanfen <stanfen@126.com>
2018-11-23 17:34:34 +0800

    Update DigraphBase.scala

--- src/main/scala/muyan/template/DigraphBase.scala
+++ src/main/scala/muyan/template/DigraphBase.scala
@@ -3,32 +3,36 @@
 import scala.collection.mutable.ListBuffer
 
  trait DigraphBase {
-   sf: EdgeGraph =>
   val prefix = "digraph clz {"
   val postfix = "}"
   val node = s"""node [ fontname = "Courier New", fontsize = 13, shape = "record" ];"""
    val portName ="head"
+   val methodName ="fun"
+   val rankDir = "rankdir = BT"
 
   //class relation token, represent interface, generalization）, Realization，Association, Aggregation,
   // Composition, Dependency ...
 //  val edge: String //mixed in digraph base
 
    //add an object to graph
-  def addItem(name: String, fun: StringBuilder, varList: StringBuilder) = {
-    val ret =  s"""$name [label = "{ <$portName> $name | <var> ${varList.toString()} | <fun> ${fun.toString()} }"];"""
+  def addItem(name: String, fun: List[String], varList: String) = {
+    val ret =  s"""$name [label = "{ <$portName> $name | <var> ${varList.toString()} | <fun> ${fun.mkString("\\l")} }"];"""
     itemBuff.prepend(ret)
   }
 
   def addRelation(parent: String, child: String, relation: String) ={
-   val t = s"$parent:$portName $relation $child:$portName"
+    val t = s"$parent:$portName -> $child:$methodName $relation"
     relationBuff.prepend(t)
+
   }
+
  private val itemBuff: ListBuffer[String] = new ListBuffer[String]
  private val relationBuff: ListBuffer[String] = new ListBuffer[String]
 
-  final def graph: String = {
+  final def graphContent: String = {
     require(itemBuff.nonEmpty )
     s"""$prefix
+       $rankDir
        |$node
        |${itemBuff.mkString("\n")}
        |${relationBuff.mkString("\n")}
