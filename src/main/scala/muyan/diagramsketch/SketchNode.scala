f5ccb95190e7c5d656b61e92752f159dfb4a8f9a
stanfen <stanfen@126.com>
2018-11-23 17:36:35 +0800

    Update SketchNode.scala

--- src/main/scala/muyan/diagramsketch/SketchNode.scala
+++ src/main/scala/muyan/diagramsketch/SketchNode.scala
@@ -8,37 +8,40 @@ sealed trait SketchNode {
   implicit def toOption[A <: SketchNode](in: A) : Option[A] = Some(in)
   implicit def isOption[A <: SketchNode](in: Option[A]) : Boolean = in.isDefined
   var deepLength = 0
-  def descSketch: String
+  def descSketch: List[String]
 }
 
 case class ClazzSketch(marker: List[Token], name: Token) extends SketchNode {
-  override def descSketch: String = {
-    name.rawText
+  override def descSketch: List[String] = {
+    List(name.rawText)
   }
 }
 //todo scan inherit class para and class body info
 //todo class define need modify for option nested with diagram lite
 // return TemplateParents not List[Token]
-case class InheritSketch(op: Option[List[Token]]) extends SketchNode {
-  override def descSketch: String = {
-   if(op.isDefined) op.get.map(_.text).mkString  else ""
+case class InheritSketch(op: List[Token]) extends SketchNode {
+  override def descSketch: List[String] = {
+//   if(op.isDefined) op.get.filter(_.tokenType.eq(Tokens.VARID)).map(_.text)  else Nil
+   op.filter(_.tokenType.eq(Tokens.VARID)).map(_.text)
   }
 }
 
 case class Slash(slh: Token) extends SketchNode {
-  override def descSketch: String = ""
+  override def descSketch: List[String] = Nil
 }
 
 case class FunctionSketch(fun: List[Token], param: ParamClauses ,returnType: Option[(Token, Type)]) extends SketchNode {
-  override def descSketch: String = {
+  override def descSketch: List[String] = {
+    println(fun)
     val name = fun.filter(_.tokenType.eq(Tokens.VARID)).map(_.text).mkString
+    println(name)
     val paraList = param.tokens.map(_.text).mkString
     val ret =  if (returnType.isDefined)  {
       val v = returnType.get
       s"${v._1.text}${v._2.tokens.map(_.text).mkString}"
     } else ""
 
-    s"$name$paraList$ret"
+    List(s"$name$paraList$ret")
   }
 }
 
