91cf7b38bab6b9f8567812e9372422d8860dbe29
stanfen <stanfen@126.com>
2018-12-27 18:57:29 +0800

    Update GraphvizTest.scala

--- src/test/scala/muyan/graphviz/GraphvizTest.scala
+++ src/test/scala/muyan/graphviz/GraphvizTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.{FlatSpec, ShouldMatchers}
 
 class GraphvizTest extends FlatSpec with ShouldMatchers{
     "gv" should "config should be read " in {
-      val gv = new Graphviz
+      val gv = new Graphviz(None, "png")
       gv.tmpDir.nonEmpty shouldBe(true)
     }
 }
