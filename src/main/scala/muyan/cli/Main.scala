21d51905627ee171f7dd8265569f724a7da10dfc
stanfen <stanfen@126.com>
2018-12-27 18:54:52 +0800

    feature: support png, jpg output format

--- src/main/scala/muyan/cli/Main.scala
+++ src/main/scala/muyan/cli/Main.scala
@@ -1,6 +1,7 @@
 package muyan.cli
 
 import java.io.File
+
 import muyan.Builder
 
 case class CliConfig(
@@ -12,56 +13,61 @@ case class CliConfig(
 object Main {
 
   //todo multi thread create graph
-  def scanDirectory(path: String): Unit = {
-   val file = new File(path)
-    buildFile(file)
+  /**
+    * @define scan dir according command line para
+    * @param para  para parsed by command line
+    * */
+  def scanDirectory(para: CliConfig): Unit = {
+   val file = new File(para.src)
+    buildFile(file, para.dst, para.format)
   }
 
   //build all
-  def buildFile(file: File): Unit = {
+  def buildFile(file: File, dst: Option[String], format: String): Unit = {
     val fileList = file.listFiles()
-    println("file list: " + fileList.size)
+
     for (in <- fileList) {
       println(in.isFile + " -> " + in.getAbsolutePath)
       if(in.isFile ) {
         if (in.getAbsolutePath.toLowerCase.endsWith(".scala")) {
-          new Builder(in.getAbsolutePath).build
+          new Builder(in.getAbsolutePath, dst, format).build
         }
       }else {
-        buildFile(in)
+        buildFile(in, dst, format)
       }
     }
   }
   //entrance of cli command
   def main(args: Array[String]): Unit = {
     var conf = CliConfig("") //default src path is empty
+    println(args.toList.mkString(" "))
     if (args.isEmpty) help()
     else {
       val itor = args.toIterator
       while (itor.hasNext){
         val i = itor.next()
-        println("print arg: "+ i)
         i match {
           case "-h" | "--help" => help()
           case "-src" => conf =  conf.copy(src = itor.next())
           case "-dst" => conf =  conf.copy(dst = Some(itor.next()))
-          case "-format" => conf =  conf.copy(format = itor.next())
+          case "-f" => conf =  conf.copy(format = if(itor.hasNext) itor.next() else "pdf")
         }
       }
     }
     println("parse param finished.")
-    println("begin to scan scala file for diagram.")
-    scanDirectory(conf.src)
-
+    println("begin to scan scala file.")
+    scanDirectory(conf)
+    println("parsing scala file finished.")
   }
 
   //usage command
   def help(): Unit  ={
-    println("scaladiagram help list:" )
+    println("scaladiagram help list:")
     println("usage: java -jar jarName <source path> [dest path] [format]" )
     println(s" -src           scala file path(required)")
     println(s" -dst           diagram file storage path")
-    println(s" -format        diagram file storage format(default:pdf) ")
+    println(s" -f             diagram file storage format,")
+    println(s"                png,pdf,jpg define,default: pdf ")
     println(s" -h,--help      show help")
 
     System.exit(1)
